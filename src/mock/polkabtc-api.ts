593008ec2c3443d5af8dbba9725dbc732bffec5c
Dominik Harz <dominik@interlay.io>
2020-09-29 21:13:57 +0200

    expose oracle and relayer api

--- src/mock/polkabtc-api.ts
+++ src/mock/polkabtc-api.ts
@@ -1,18 +1,22 @@
 import { KeyringPair } from "@polkadot/keyring/types";
 import { ApiPromise } from "@polkadot/api";
 
-import { IssueAPI, RedeemAPI, VaultsAPI, StakedRelayerAPI } from "../apis";
+import { IssueAPI, RedeemAPI, VaultsAPI, StakedRelayerAPI, OracleAPI } from "../apis";
 import { MockIssueAPI } from "./apis/issue";
 import { MockRedeemAPI } from "./apis/redeem";
 import { MockVaultsAPI } from "./apis/vaults";
 import { MockStakedRelayerAPI } from "./apis/staked-relayer";
 import { PolkaBTCAPI } from "../polkabtc-api";
+import { StakedRelayerClient } from "../http";
+import { MockOracleAPI } from "./apis/oracle";
 
 export default class MockPolkaBTCAPI implements PolkaBTCAPI {
     public readonly vaults: VaultsAPI;
     public readonly issue: IssueAPI;
     public readonly redeem: RedeemAPI;
     public readonly stakedRelayer: StakedRelayerAPI;
+    public readonly relayer: StakedRelayerClient;
+    public readonly oracle: OracleAPI;
 
     private account?: KeyringPair;
 
@@ -21,6 +25,8 @@ export default class MockPolkaBTCAPI implements PolkaBTCAPI {
         this.issue = new MockIssueAPI();
         this.redeem = new MockRedeemAPI();
         this.stakedRelayer = new MockStakedRelayerAPI();
+        this.relayer = new StakedRelayerClient("");
+        this.oracle = new MockOracleAPI();
     }
 
     setAccount(account: KeyringPair): void {
