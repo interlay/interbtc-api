462adf6ce0b38364217a670e7250f630c2fe53f7
Dominik Harz <dominik@interlay.io>
2020-10-05 21:54:27 +0900

    Update src/utils/bitcoin.ts
    Co-authored-by: Greg Hill <gregorydhill@outlook.com>

--- src/utils/bitcoin.ts
+++ src/utils/bitcoin.ts
@@ -5,7 +5,7 @@ export function getP2WPKHFromH160(hash: string): string | undefined {
     try {
         // TODO: specify script format in parachain
         const payment = bitcoin.payments.p2wpkh({
-            hash: Buffer.from(hash),
+            hash: Buffer.from(hash.buffer),
             network: bitcoin.networks.testnet,
         });
         btcAddress = payment.address;
