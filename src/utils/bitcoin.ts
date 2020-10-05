fecc95f8c6aa49cbdafefdd3ad3bea822113747a
Dominik Harz <dominik@interlay.io>
2020-10-05 14:57:54 +0200

    add misiing import

--- src/utils/bitcoin.ts
+++ src/utils/bitcoin.ts
@@ -1,4 +1,5 @@
 import * as bitcoin from "bitcoinjs-lib";
+import { H160 } from "@polkadot/types/interfaces";
 
 export function getP2WPKHFromH160(hash: H160): string | undefined {
     let btcAddress: string | undefined;
