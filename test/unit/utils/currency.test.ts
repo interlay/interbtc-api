1fe6c31d0ae797af0f5e5f1e72dd0f2b96958772
Dominik Harz <dominik@interlay.io>
2020-10-07 10:09:21 +0200

    add util functions

--- test/unit/utils/currency.test.ts
+++ test/unit/utils/currency.test.ts
@@ -1,7 +1,14 @@
-import { btcToSat, satToBTC, dotToPlanck, planckToDOT } from "../../../src/utils";
+import { btcToSat, satToMBTC, satToBTC, dotToPlanck, planckToDOT, roundTwoDecimals } from "../../../src/utils";
 import { assert } from "../../chai";
 
 describe("currency", () => {
+    describe("Bitcoin", () => {
+        it("should round to two decimals", () => {
+            const bitcoin = "13.5132132132";
+            const rounded = "13.51";
+            assert.equal(roundTwoDecimals(bitcoin), rounded);
+        })
+    })
     describe("Bitcoin", () => {
         it("should convert a Bitcoin string to a Satoshi string", () => {
             const bitcoin = "13.5";
@@ -22,6 +29,11 @@ describe("currency", () => {
             const satoshi = "13500000000000";
             assert.equal(satToBTC(satoshi), bitcoin);
         })
+        it("should convert a Satoshi string to a mBTC string", () => {
+            const mbtc = "135000000";
+            const satoshi = "13500000000000";
+            assert.equal(satToMBTC(satoshi), mbtc);
+        })
     })
     describe("Dot", () => {
         it("should convert a Dot string to a Planck string", () => {
@@ -41,6 +53,16 @@ describe("currency", () => {
         it("should convert a Planck string to a Dot string", () => {
             const dot = "135000";
             const planck = "1350000000000000";
+            assert.equal(planckToDOT(planck), dot);
+        })
+        it("should convert a Planck string to a Dot string", () => {
+            const dot = "0.0000000135";
+            const planck = "135";
+            assert.equal(planckToDOT(planck), dot);
+        })
+        it("should convert a Planck string to a Dot string", () => {
+            const dot = "0.000000001";
+            const planck = "10";
             assert.equal(planckToDOT(planck), dot);
         })
     })
