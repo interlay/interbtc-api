1134eb5d44e2721528514765236563b4f79d2e04
is319 <is319@ic.ac.uk>
2020-10-26 15:06:14 +0200

    Add constants api calls

---
+++ src/apis/constants.ts
@@ -0,0 +1,109 @@
+import { Balance, BalanceOf, BlockNumber, Moment, RuntimeDbWeight, Weight } from "@polkadot/types/interfaces/runtime";
+import { DOT } from "@interlay/polkabtc/interfaces/default";
+import { ApiPromise } from "@polkadot/api";
+import { u32, u64 } from "@polkadot/types/primitive";
+import { Vec } from "@polkadot/types/codec";
+import { WeightToFeeCoefficient } from "@polkadot/types/interfaces/support";
+
+export interface ConstantsAPI {
+    getDotExistentialDeposit(): Balance;
+    getIssuePeriod(): BlockNumber;
+    getPolkaBtcExistentialDeposit(): Balance;
+    getReplacePeriod(): BlockNumber;
+    getStakedRelayersMaturityPeriod(): BlockNumber;
+    getStakedRelayersMinimumDeposit(): DOT;
+    getStakedRelayersMinimumParticipants(): u64;
+    getStakedRelayersMinimumStake(): DOT;
+    getStakedRelayersVoteThreshold(): u64;
+    getStakedRelayersVotingPeriod(): BlockNumber;
+    getSystemBlockExecutionWeight(): Weight;
+    getSystemBlockHashCount(): BlockNumber;
+    getSystemDbWeight(): RuntimeDbWeight;
+    getSystemExtrinsicBaseWeight(): Weight;
+    getSystemMaximumBlockLength(): u32;
+    getSystemMaximumBlockWeight(): Weight;
+    getTimestampMinimumPeriod(): Moment;
+    getTransactionByteFee(): BalanceOf;
+    getTransactionWeightToFee(): Vec<WeightToFeeCoefficient>;
+}
+
+export class DefaultConstantsAPI implements ConstantsAPI {
+    constructor(private api: ApiPromise) {}
+
+    getDotExistentialDeposit(): Balance {
+        return this.api.consts.dot.existentialDeposit;
+    }
+
+    getIssuePeriod(): BlockNumber {
+        return this.api.consts.issue.issuePeriod;
+    }
+
+    getPolkaBtcExistentialDeposit(): Balance {
+        return this.api.consts.polkaBtc.existentialDeposit;
+    }
+
+    getReplacePeriod(): BlockNumber {
+        return this.api.consts.replace.replacePeriod;
+    }
+
+    getStakedRelayersMaturityPeriod(): BlockNumber {
+        return this.api.consts.stakedRelayers.maturityPeriod;
+    }
+
+    getStakedRelayersMinimumDeposit(): DOT {
+        return this.api.consts.stakedRelayers.minimumDeposit;
+    }
+
+    getStakedRelayersMinimumParticipants(): u64 {
+        return this.api.consts.stakedRelayers.minimumParticipants;
+    }
+
+    getStakedRelayersMinimumStake(): DOT {
+        return this.api.consts.stakedRelayers.minimumStake;
+    }
+
+    getStakedRelayersVoteThreshold(): u64 {
+        return this.api.consts.stakedRelayers.voteThreshold;
+    }
+
+    getStakedRelayersVotingPeriod(): BlockNumber {
+        return this.api.consts.stakedRelayers.votingPeriod;
+    }
+
+    getSystemBlockExecutionWeight(): Weight {
+        return this.api.consts.system.blockExecutionWeight;
+    }
+
+    getSystemBlockHashCount(): BlockNumber {
+        return this.api.consts.system.blockHashCount;
+    }
+
+    getSystemDbWeight(): RuntimeDbWeight {
+        return this.api.consts.system.dbWeight;
+    }
+
+    getSystemExtrinsicBaseWeight(): Weight {
+        return this.api.consts.system.extrinsicBaseWeight;
+    }
+
+    getSystemMaximumBlockLength(): u32 {
+        return this.api.consts.system.maximumBlockLength;
+    }
+
+    getSystemMaximumBlockWeight(): Weight {
+        return this.api.consts.system.maximumBlockWeight;
+    }
+
+    getTimestampMinimumPeriod(): Moment {
+        return this.api.consts.timestamp.minimumPeriod;
+    }
+
+    getTransactionByteFee(): BalanceOf {
+        return this.api.consts.transactionPayment.transactionByteFee;
+    }
+
+    getTransactionWeightToFee(): Vec<WeightToFeeCoefficient> {
+        return this.api.consts.transactionPayment.weightToFee;
+    }
+
+}
