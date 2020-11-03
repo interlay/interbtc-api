eff6352177c451019ba9f14382d31950024e1d20
Daniel Savu <savudani04@yahoo.ro>
2020-11-03 15:05:50 +0200

    feat: Query btc txs by op_return

---
+++ test/unit/apis/btc-core.test.ts
@@ -0,0 +1,43 @@
+import { ApiPromise } from "@polkadot/api";
+import sinon from "sinon";
+import { createPolkadotAPI } from "../../../src/factory";
+import { assert } from "../../chai";
+import { defaultEndpoint } from "../../config";
+import { BTCCoreAPI } from "../../../src/apis";
+import { DefaultBTCCoreAPI } from "../../../src/apis/btc-core";
+
+// fails to disconnect from the api
+describe.skip("btc-core", () => {
+    let api: ApiPromise;
+    let btcCore: BTCCoreAPI;
+    let sandbox: sinon.SinonSandbox;
+
+    before(async () => {
+        api = await createPolkadotAPI("mock");
+        sandbox = sinon.createSandbox();
+    });
+
+    beforeEach(async () => {
+        api = await createPolkadotAPI(defaultEndpoint);
+        btcCore = new DefaultBTCCoreAPI(false);
+    });
+
+    after(() => {
+        return api.disconnect();
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it("should reject getTxByOpcode if query returns empty array", async () => {
+        sandbox.stub(DefaultBTCCoreAPI.prototype, "getData").returns(Promise.resolve([]));
+        assert.isRejected(btcCore.getTxIdByOpcode("random"));
+    });
+
+    it("should reject getTxByOpcode if query returns array longer than 1", async () => {
+        sandbox.stub(DefaultBTCCoreAPI.prototype, "getData").returns(Promise.resolve([undefined, undefined]));
+        assert.isRejected(btcCore.getTxIdByOpcode("random"));
+    });
+
+});
