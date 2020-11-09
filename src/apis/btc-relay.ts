86c4c40ba1b3849b35d66295a04aebf5a35c0233
Gregory Hill <gregorydhill@outlook.com>
2020-11-09 15:00:53 +0000

    feat(btc-relay): get stable confirmations param from btc-relay
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- src/apis/btc-relay.ts
+++ src/apis/btc-relay.ts
@@ -2,11 +2,16 @@ import { ApiPromise } from "@polkadot/api";
 import { BTCCoreAPI } from "./btc-core";
 
 export interface BTCRelayAPI {
+    getStableBitcoinConfirmations(): Promise<number>;
     verifyTransactionInclusion(txid: string, confirmations?: number, insecure?: boolean): Promise<void>;
 }
 
 export class DefaultBTCRelayAPI implements BTCRelayAPI {
     constructor(private api: ApiPromise, private btcCore: BTCCoreAPI) {}
+
+    async getStableBitcoinConfirmations(): Promise<number> {
+        return this.api.query.btcRelay.stableBitcoinConfirmations().then((param) => param.toNumber());
+    }
 
     async verifyTransactionInclusion(
         txid: string,
