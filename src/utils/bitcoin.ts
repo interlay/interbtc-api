46fdebbd1aaca469d5791d24067a4aeeb0b43692
Gregory Hill <gregorydhill@outlook.com>
2020-11-10 12:18:08 +0000

    feat(bitcoin): util getP2WPKHFromH160 should take network param
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- src/utils/bitcoin.ts
+++ src/utils/bitcoin.ts
@@ -1,17 +1,19 @@
 import * as bitcoin from "bitcoinjs-lib";
 import { H160 } from "@polkadot/types/interfaces";
 
-export function getP2WPKHFromH160(hash: H160): string | undefined {
+export { Network } from "bitcoinjs-lib";
+
+export function getP2WPKHFromH160(hash: H160, network: bitcoin.Network): string | undefined {
     let btcAddress: string | undefined;
     try {
         // TODO: specify script format in parachain
         const payment = bitcoin.payments.p2wpkh({
             hash: Buffer.from(hash.buffer),
-            network: bitcoin.networks.testnet,
+            network,
         });
         btcAddress = payment.address;
-    } catch (error) {
-        console.log("Error converting BTC address " + hash);
+    } catch (err) {
+        console.log(`Error converting BTC address ${hash}: ${err}`);
     }
     return btcAddress;
 }
