0b9c14bbbc2a6e4f41d387aae81575ca07545566
Daniel Savu <savudani04@yahoo.ro>
2021-03-09 14:12:52 +0000

    fix(BTC-Core): test with spent output

--- test/integration/external/btc-core.test.ts
+++ test/integration/external/btc-core.test.ts
@@ -80,10 +80,10 @@ describe("BTCCore testnet", function () {
 
     describe("get tx by recipient", () => {
         it("should return correct tx id when called with amount and receiver", async () => {
-            const recipientAddress = "tb1qt49nhv6cqx5edp85awwd4a40fw0mh7cln0v9s0";
-            const amountAsBTC = "0.00012364";
+            const recipientAddress = "tb1q9dxnjz0qwh7yj6axl0q9r7lyc9n3gat8nlrvhf";
+            const amountAsBTC = "0.0001236";
             const txid = await btcCore.getTxIdByRecipientAddress(recipientAddress, amountAsBTC);
-            assert.strictEqual(txid, "97c4a73a971fd9410416ee46bc9bd62f08e68efc35d4f52c5b4a50307d5f6b93");
+            assert.strictEqual(txid, "41640c7703ebd972dd913f89c6d66941894d03ef3934edc59259342c7cc8126e");
         });
     });
 
