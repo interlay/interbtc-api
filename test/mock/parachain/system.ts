d3b8f94ffbf152b6c8c776a9c5efc390b9cc5040
Dominik Harz <dominik@interlay.io>
2021-05-27 22:18:17 +0200

    chore: mock api additions

--- test/mock/parachain/system.ts
+++ test/mock/parachain/system.ts
@@ -1,10 +1,10 @@
 import { SystemAPI } from "../../../src/parachain/system";
-import { BlockNumber } from "@polkadot/types/interfaces/runtime";
-import { TypeRegistry, u32 } from "@polkadot/types";
 
 export class MockSystemAPI implements SystemAPI {
-    async getCurrentBlockNumber(): Promise<BlockNumber> {
-        const registry = new TypeRegistry();
-        return new u32(registry, 44) as BlockNumber;
+    async getCurrentBlockNumber(): Promise<number> {
+        return 44;
+    }
+    async getCurrentActiveBlockNumber(): Promise<number> {
+        return 42;
     }
 }
