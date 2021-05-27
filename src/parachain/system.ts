81b278e2b4ac260cb638cf920de368e36f1032da
Dominik Harz <dominik@interlay.io>
2021-05-27 21:58:10 +0200

    feat: add active block number

--- src/parachain/system.ts
+++ src/parachain/system.ts
@@ -8,14 +8,23 @@ export interface SystemAPI {
     /**
      * @returns The current block number being processed.
      */
-    getCurrentBlockNumber(): Promise<BlockNumber>;
+    getCurrentBlockNumber(): Promise<number>;
+    /**
+     * @returns The current active block number being processed.
+     */
+    getCurrentBlockNumber(): Promise<number>;
 }
 
 export class DefaultSystemAPI implements SystemAPI {
     constructor(private api: ApiPromise) { }
 
-    async getCurrentBlockNumber(): Promise<BlockNumber> {
+    async getCurrentBlockNumber(): Promise<number> {
         const head = await this.api.rpc.chain.getFinalizedHead();
-        return await this.api.query.system.number.at(head);
+        return await (await this.api.query.system.number.at(head)).toNumber();
+    }
+
+    async getCurrentActiveBlockNumber(): Promise<number> {
+        const head = await this.api.rpc.chain.getFinalizedHead();
+        return await (await this.api.query.security.activeBlockCount.at(head)).toNumber();
     }
 }
