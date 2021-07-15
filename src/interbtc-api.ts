8c8c89dff252621c38b8783367c3f890ac51b4ee
Theodore Bugnet <theodore@interlay.io>
2021-07-15 18:05:49 +0100

    feat(index): added module exposing interbtc-index, allowing for explicit wrappers as desired and auto-generating thin wrappers for every other API in index

--- src/interbtc-api.ts
+++ src/interbtc-api.ts
@@ -19,6 +19,8 @@ import { DefaultReplaceAPI, ReplaceAPI } from "./parachain/replace";
 import { Network, networks } from "bitcoinjs-lib";
 import { BitcoinNetwork } from "./types/bitcoinTypes";
 import { DefaultIndexAPI, IndexAPI } from "./external/interbtc-index";
+import { INDEX_URL } from "./utils/constants";
+import { Configuration as IndexConfiguration } from "@interlay/interbtc-index-client";
 
 export * from "./factory";
 export * from "./parachain/transaction";
@@ -93,7 +95,7 @@ export class DefaultInterBTCAPI implements InterBTCAPI {
         this.issue = new DefaultIssueAPI(api, btcNetwork, this.electrsAPI, _account);
         this.redeem = new DefaultRedeemAPI(api, btcNetwork, this.electrsAPI, _account);
         this.nomination = new DefaultNominationAPI(api, btcNetwork, this.electrsAPI, _account);
-        this.index = DefaultIndexAPI;
+        this.index = DefaultIndexAPI(new IndexConfiguration({ basePath: INDEX_URL }));
     }
 
     setAccount(account: AddressOrPair, signer?: Signer): void {
