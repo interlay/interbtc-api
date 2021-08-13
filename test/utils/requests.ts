70f9455aa5724b9c02a48e8853289fba533a462a
Daniel Savu <savudani04@yahoo.ro>
2021-08-13 11:32:23 +0100

    chore: Bump version

--- test/utils/requests.ts
+++ test/utils/requests.ts
@@ -6,5 +6,9 @@ export function assertRequestListsEqual(list1: { id: string }[], list2: { id: st
         1,
         "Error in docker-compose setup. Should have at least 1 issue request"
     );
-    assert.equal(list1[0].id, list2[0].id, "Cached issue request ID does not exist");
+    let parsedList1 = list1.map(v => v.id);
+    let parsedList2 = list2.map(v => v.id);
+    for (const id of parsedList1) {
+        assert.isTrue(parsedList2.includes(id), "ID mismatch between cached and actual list items");
+    }
 }
