1d305fed9bcaabad804d91e2fb31884c7a831d3c
Daniel Savu <savudani04@yahoo.ro>
2022-01-27 14:02:36 +0000

    chore: move unused scripts outside `src`

--- scripts/xcm-cross-chain-transfer.ts
+++ scripts/xcm-cross-chain-transfer.ts
@@ -1,9 +1,9 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
-import { createPolkadotAPI } from "../factory";
+import { createPolkadotAPI } from "../src/factory";
 import { Keyring } from "@polkadot/api";
 import {
     DefaultTransactionAPI,
-} from "../parachain";
+} from "../src/parachain";
 import { cryptoWaitReady } from "@polkadot/util-crypto";
 import { XcmVersionedMultiLocation } from "@polkadot/types/lookup";
 import { XcmV1MultiLocation } from "@polkadot/types/lookup";
