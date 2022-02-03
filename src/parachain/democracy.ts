5a9c9d0ab0aea15cae62a65eb4fd145d4a407323
Thomas Jeatt <hello@tomjeatt.com>
2022-02-03 16:20:52 +0000

    feature: add additional democracy methods

--- src/parachain/democracy.ts
+++ src/parachain/democracy.ts
@@ -1,4 +1,5 @@
 import { ApiPromise } from "@polkadot/api";
+import { DemocracyVote } from "@polkadot/types/lookup";
 
 import { addHexPrefix } from "../utils/encoding";
 import { TransactionAPI } from "..";
@@ -23,6 +24,35 @@ export interface DemocracyAPI {
         proposalIndex: number,
         delay: number
     ): Promise<void>;
+    /**
+     * @param proposalHash
+     * @param delay
+     */
+    propose(
+        proposalHash: string,
+        delay: number
+    ): Promise<void>;
+    /**
+     * @param proposalHash
+     * @param delay
+     */
+    second(
+        proposalHash: string,
+        upperBound: number
+    ): Promise<void>;
+    /**
+     * @param referendumIndex
+     */
+    vote(
+        referendumIndex: number,
+        vote: DemocracyVote
+    ): Promise<void>;
+    /**
+     * @param referendumIndex
+     */
+    removeVote(
+        referendumIndex: number
+    ): Promise<void>;
 }
 
 export class DefaultDemocracyAPI implements DemocracyAPI {
@@ -45,6 +75,39 @@ export class DefaultDemocracyAPI implements DemocracyAPI {
         delay: number
     ): Promise<void> {
         const tx = this.api.tx.democracy.fastTrack(proposalIndex, delay);
+        await this.transactionAPI.sendLogged(tx, this.api.events.democracy.Passed);
+    }
+
+    async propose(
+        proposalHash: string,
+        delay: number
+    ): Promise<void> {
+        const parsedHash = this.api.createType("H256", addHexPrefix(proposalHash));
+        const tx = this.api.tx.democracy.propose(parsedHash, delay);
+        await this.transactionAPI.sendLogged(tx, this.api.events.democracy.Passed);
+    }
+
+    async second(
+        proposalHash: string,
+        upperBound: number
+    ): Promise<void> {
+        const parsedHash = this.api.createType("H256", addHexPrefix(proposalHash));
+        const tx = this.api.tx.democracy.second(parsedHash, upperBound);
+        await this.transactionAPI.sendLogged(tx, this.api.events.democracy.Passed);
+    }
+
+    async vote(
+        referendumIndex: number,
+        vote: DemocracyVote
+    ): Promise<void> {
+        const tx = this.api.tx.democracy.vote(referendumIndex, vote);
+        await this.transactionAPI.sendLogged(tx, this.api.events.democracy.Passed);
+    }
+
+    async removeVote(
+        referendumIndex: number
+    ): Promise<void> {
+        const tx = this.api.tx.democracy.removeVote(referendumIndex);
         await this.transactionAPI.sendLogged(tx, this.api.events.democracy.Passed);
     }
 
