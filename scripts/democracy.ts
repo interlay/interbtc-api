c6284e540eff7a3658f5b5172c820748f07a2fca
Sander Bosma <sanderbosma@gmail.com>
2022-02-15 11:51:05 +0100

    chore: script additions and changes

---
+++ scripts/democracy.ts
@@ -0,0 +1,62 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
+import { createSubstrateAPI } from "../src/factory";
+import { ApiPromise, Keyring } from "@polkadot/api";
+import {
+    DefaultTransactionAPI,
+} from "../src/parachain";
+import { cryptoWaitReady } from "@polkadot/util-crypto";
+import { DemocracyVote } from "@polkadot/types/lookup";
+import type { 
+    BTreeMap, Bytes, Compact, Enum, Null, Option, Result, Struct, Text, U256, U8aFixed, Vec, bool, i128, i32, i64, u128, u16, u32, u64, u8
+} from "@polkadot/types";
+
+// const PARACHAIN_ENDPOINT = "wss://api-dev-kintsugi.interlay.io/parachain";
+// const ACCOUNT_URI = "quick sense network ozone ostrich bone hole possible timber clog urban primary//account/1";
+
+
+const PARACHAIN_ENDPOINT = "ws://127.0.0.1:9988";
+const ACCOUNT_URI = "//Alice";
+
+main().catch((err) => {
+    console.log("Error thrown by script:");
+    console.log(err);
+});
+
+async function main(): Promise<void> {
+    const rawData = require('fs').readFileSync('testnet_runtime_parachain.compact.compressed.wasm');
+    const data = '0x' + rawData.toString('hex');
+    await cryptoWaitReady();
+    const keyring = new Keyring({ type: "sr25519" });
+    const userKeyring = keyring.addFromUri(ACCOUNT_URI);
+    const api = await createSubstrateAPI(PARACHAIN_ENDPOINT);
+
+    const transactionAPI = new DefaultTransactionAPI(api, userKeyring);
+    
+    const proposal = api.tx.parachainSystem.authorizeUpgrade('0x72e1663c5c98310ff9654700738b4f2987a7d8d9d4ccbfd39babe98517ddd7eb');
+    const proposalData = proposal.method.toHex();
+    const proposalHash = proposal.method.hash.toHex();
+
+
+    // // sudo-upgrade using set-code
+    // await transactionAPI.sendLogged(api.tx.sudo.sudoUncheckedWeight(api.tx.system.setCode(data), 0), undefined);
+
+    console.log("Locking KINT...");
+    await transactionAPI.sendLogged(api.tx.escrow.createLock(10000000000000, 2628000), undefined);
+
+    console.log("Submitting preimage...");
+    await transactionAPI.sendLogged(api.tx.democracy.notePreimage(proposalData), undefined);
+
+    console.log("Submitting proposal...");
+    await transactionAPI.sendLogged(api.tx.democracy.propose(proposalHash, 5000000000000), undefined);
+
+    console.log("Fastracking proposal...");
+    await transactionAPI.sendLogged(api.tx.sudo.sudo(api.tx.democracy.fastTrack(0, 3)), undefined);
+
+    console.log("Voting on proposal...");
+    await transactionAPI.sendLogged(api.tx.democracy.vote(0, api.createType<DemocracyVote>("DemocracyVote", {
+            aye: true,
+            balance: 1000000000000
+        })), undefined);
+
+    api.disconnect();
+}
--- scripts/xcm-return-unknown-tokens.ts
+++ scripts/xcm-return-unknown-tokens.ts
@@ -6,10 +6,11 @@ import {
 } from "../src/parachain";
 import { cryptoWaitReady } from "@polkadot/util-crypto";
 import { XcmVersionedMultiLocation } from "@polkadot/types/lookup";
+import { XcmVersionedMultiAsset } from "@polkadot/types/lookup";
 import { XcmV1MultiLocation } from "@polkadot/types/lookup";
 
 
-const PARACHAIN_ENDPOINT = "ws://127.0.0.1:9988";
+const PARACHAIN_ENDPOINT = "ws://127.0.0.1:9999";
 const ACCOUNT_URI = "//Alice";
 
 main().catch((err) => {
@@ -26,11 +27,26 @@ async function main(): Promise<void> {
 
     const transactionAPI = new DefaultTransactionAPI(api, userKeyring);
 
-    const currencyId = api.createType("InterbtcPrimitivesCurrencyId", { 
-        token: api.createType("InterbtcPrimitivesTokenSymbol", {
-            kint: true,
-        }) 
-    });
+    const asset = api.createType<XcmVersionedMultiAsset>("XcmVersionedMultiAsset", {
+        v1: api.createType("XcmV1MultiAsset", {
+            id: api.createType("XcmV1MultiassetAssetId", {
+                concrete: api.createType("XcmV1MultiLocation", {
+                    parents: 1,
+                    interior: api.createType("XcmV1MultilocationJunctions", {
+                        x2: [
+                            api.createType("XcmV1Junction", { 
+                                parachain: 2000 
+                            }), api.createType("XcmV1Junction", { 
+                                generalKey: [0, 12] // kint
+                            })]
+                    })
+                })
+            }),
+            fun: api.createType("XcmV1MultiassetFungibility", {
+                fungible: 10000000000
+            })
+        })}
+    );
 
     const dest = api.createType<XcmVersionedMultiLocation>("XcmVersionedMultiLocation", {
         v1: api.createType("XcmV1MultiLocation", {
@@ -38,11 +54,11 @@ async function main(): Promise<void> {
             interior: api.createType("XcmV1MultilocationJunctions", {
                 x2: [
                     api.createType("XcmV1Junction", { 
-                        parachain: 3000 
+                        parachain: 2000 
                     }), api.createType("XcmV1Junction", { 
                         accountId32: {
                             network: api.createType("XcmV0JunctionNetworkId", { any: true }),
-                            id: "0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d" // alice 
+                            id: "0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48" // bob 
                         }
                     })]
             })
@@ -53,13 +69,12 @@ async function main(): Promise<void> {
     const sibling = api.createType<XcmV1MultiLocation>("XcmV1MultiLocation", {
         parents: 1,
         interior: api.createType("XcmV1MultilocationJunctions", {
-            x1: api.createType("XcmV1Junction", { parachain: 3000 })
+            x1: api.createType("XcmV1Junction", { parachain: 2000 })
         })
     });
 
     const setupTx = api.tx.sudo.sudo(api.tx.polkadotXcm.forceXcmVersion(sibling, 2));
-    const xcmTx = api.tx.xTokens.transfer(currencyId, 100000000000, dest, 400000000000);
-    // send back 10000000000, 1000000001
+    const xcmTx = api.tx.xTokens.transferMultiasset(asset, dest, 1000000001);
 
     console.log("Constructed the tx, broadcasting first...");
     await transactionAPI.sendLogged(setupTx, undefined);
