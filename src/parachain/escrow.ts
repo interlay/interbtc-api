b4a51db6e8176b19562e34b565a185e819abd399
Thomas Jeatt <hello@tomjeatt.com>
2022-04-06 10:31:14 +0100

    refactor: update interface

--- src/parachain/escrow.ts
+++ src/parachain/escrow.ts
@@ -107,6 +107,11 @@ export interface EscrowAPI {
             amount: MonetaryAmount<Currency<U>, U>,
             apy: Big
     }>;
+    /**
+     * @remarks Returns total stake as governance token amount (e.g. KINT or INTR)
+     */
+    getEscrowTotalStakeAsGovernanceToken(): Promise<MonetaryAmount<Currency<GovernanceUnit>, GovernanceUnit>>;
+
 }
 
 export class DefaultEscrowAPI implements EscrowAPI {
