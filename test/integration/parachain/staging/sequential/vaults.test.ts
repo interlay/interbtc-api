f79bb9871734ac1dd7fc50546b9168f42b72d1fd
Brendon Votteler <bvotteler@users.noreply.github.com>
2022-05-20 13:53:30 +0100

    fix (test): Use Big.gt() instead of > operator

--- test/integration/parachain/staging/sequential/vaults.test.ts
+++ test/integration/parachain/staging/sequential/vaults.test.ts
@@ -138,7 +138,7 @@ describe("vaultsAPI", () => {
             throw new Error("Collateralization is undefined");
         }
         assert.isTrue(
-            collateralizationAfterDeposit > collateralizationBeforeDeposit,
+            collateralizationAfterDeposit.gt(collateralizationBeforeDeposit),
             `Depositing did not increase collateralization,
             expected ${collateralizationAfterDeposit} greater than ${collateralizationBeforeDeposit}`
         );
@@ -150,7 +150,7 @@ describe("vaultsAPI", () => {
             throw new Error("Collateralization is undefined");
         }
         assert.isTrue(
-            collateralizationAfterDeposit > collateralizationAfterWithdrawal,
+            collateralizationAfterDeposit.gt(collateralizationAfterWithdrawal),
             `Withdrawing did not decrease collateralization, expected
             ${collateralizationAfterDeposit} greater than ${collateralizationAfterWithdrawal}`
         );
