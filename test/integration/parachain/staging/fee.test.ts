43fcd0572708b8e6dfecae67146d8e3ae33f52e7
Brendon Votteler <bvotteler@users.noreply.github.com>
2022-05-26 13:56:00 +0100

    chore (test): update fee.test.ts to use multi-collateral currencies

--- test/integration/parachain/staging/fee.test.ts
+++ test/integration/parachain/staging/fee.test.ts
@@ -5,14 +5,21 @@ import Big from "big.js";
 
 import { createSubstrateAPI } from "../../../../src/factory";
 import { ESPLORA_BASE_PATH, ORACLE_URI, PARACHAIN_ENDPOINT } from "../../../config";
-import { CollateralUnit, DefaultInterBtcApi, getCorrespondingCollateralCurrencies, InterBtcApi, newMonetaryAmount, WrappedCurrency } from "../../../../src";
+import { 
+    CollateralUnit, 
+    DefaultInterBtcApi, 
+    getCorrespondingCollateralCurrencies, 
+    InterBtcApi, 
+    newMonetaryAmount, 
+    WrappedCurrency 
+} from "../../../../src";
 import { GriefingCollateralType } from "../../../../src/parachain/fee";
 import { callWithExchangeRate } from "../../../utils/helpers";
 
 describe("fee", () => {
     let api: ApiPromise;
     let oracleInterBtcAPI: InterBtcApi;
-    let collateralCurrency: Currency<CollateralUnit>;
+    let collateralCurrencies: Array<Currency<CollateralUnit>>;
     let wrappedCurrency: WrappedCurrency;
 
     before(async function () {
@@ -20,7 +27,8 @@ describe("fee", () => {
         const keyring = new Keyring({ type: "sr25519" });
         const oracleAccount = keyring.addFromUri(ORACLE_URI);
         oracleInterBtcAPI = new DefaultInterBtcApi(api, "regtest", oracleAccount, ESPLORA_BASE_PATH);
-        collateralCurrency = getCorrespondingCollateralCurrencies(oracleInterBtcAPI.getGovernanceCurrency())[0] as Currency<CollateralUnit>;
+        collateralCurrencies = 
+            getCorrespondingCollateralCurrencies(oracleInterBtcAPI.getGovernanceCurrency()) as Array<Currency<CollateralUnit>>;
         wrappedCurrency = oracleInterBtcAPI.getWrappedCurrency();
     });
 
@@ -34,32 +42,37 @@ describe("fee", () => {
     }).timeout(2000000);
 
     it("should getGriefingCollateral for issue", async () => {
-        const exchangeRateValue = new Big("3855.23187");
-        const exchangeRate = new ExchangeRate<
-            Bitcoin,
-            BitcoinUnit,
-            typeof collateralCurrency,
-            typeof collateralCurrency.units
-        >(Bitcoin, collateralCurrency, exchangeRateValue);
-        await callWithExchangeRate(oracleInterBtcAPI.oracle, exchangeRate, async () => {
-            const amountBtc = newMonetaryAmount(0.001, wrappedCurrency, true);
-            const griefingCollateral = await oracleInterBtcAPI.fee.getGriefingCollateral(amountBtc, GriefingCollateralType.Issue);
-            assert.equal(griefingCollateral.toBig(griefingCollateral.currency.base).round(5, 0).toString(), "0.00001");
-        });
-    }).timeout(200000);
+        for (const collateralCurrency of collateralCurrencies) {
+            const exchangeRateValue = new Big("3855.23187");
+            const exchangeRate = new ExchangeRate<
+                Bitcoin,
+                BitcoinUnit,
+                typeof collateralCurrency,
+                typeof collateralCurrency.units
+            >(Bitcoin, collateralCurrency, exchangeRateValue);
+            await callWithExchangeRate(oracleInterBtcAPI.oracle, exchangeRate, async () => {
+                const amountBtc = newMonetaryAmount(0.001, wrappedCurrency, true);
+                const griefingCollateral = await oracleInterBtcAPI.fee.getGriefingCollateral(amountBtc, GriefingCollateralType.Issue);
+                assert.equal(griefingCollateral.toBig(griefingCollateral.currency.base).round(5, 0).toString(), "0.00001");
+            });
+        }
+    }).timeout(400000);
 
     it("should getGriefingCollateral for replace", async () => {
-        const exchangeRateValue = new Big("3855.23187");
-        const exchangeRate = new ExchangeRate<
-            Bitcoin,
-            BitcoinUnit,
-            typeof collateralCurrency,
-            typeof collateralCurrency.units
-            >(Bitcoin, collateralCurrency, exchangeRateValue);
-        await callWithExchangeRate(oracleInterBtcAPI.oracle, exchangeRate, async () => {
-            const amountToReplace = newMonetaryAmount(0.728, wrappedCurrency, true);
-            const griefingCollateral = await oracleInterBtcAPI.fee.getGriefingCollateral(amountToReplace, GriefingCollateralType.Replace);
-            assert.equal(griefingCollateral.toString(griefingCollateral.currency.base), "16.744");
-        });
-    }).timeout(200000);
+        for (const collateralCurrency of collateralCurrencies) {
+            const exchangeRateValue = new Big("3855.23187");
+            const exchangeRate = new ExchangeRate<
+                Bitcoin,
+                BitcoinUnit,
+                typeof collateralCurrency,
+                typeof collateralCurrency.units
+                >(Bitcoin, collateralCurrency, exchangeRateValue);
+            await callWithExchangeRate(oracleInterBtcAPI.oracle, exchangeRate, async () => {
+                const amountToReplace = newMonetaryAmount(0.728, wrappedCurrency, true);
+                const griefingCollateral = 
+                    await oracleInterBtcAPI.fee.getGriefingCollateral(amountToReplace, GriefingCollateralType.Replace);
+                assert.equal(griefingCollateral.toString(griefingCollateral.currency.base), "16.744");
+            });
+        }
+    }).timeout(400000);
 });
