0878efaeae8421f31df8c95e49104c4805cbfea1
Brendon Votteler <bvotteler@users.noreply.github.com>
2022-10-20 12:32:06 +0200

    fix: correct parsing of pending vs expired status on redeem requests

--- test/unit/utils/encoding.test.ts
+++ test/unit/utils/encoding.test.ts
@@ -1,8 +1,19 @@
 import { TypeRegistry } from "@polkadot/types";
 import { assert } from "../../chai";
 import { getAPITypes } from "../../../src/factory";
-import { reverseEndianness, uint8ArrayToString, stripHexPrefix, reverseEndiannessHex } from "../../../src/utils";
+import { RedeemStatus } from "../../../src/types";
+import {
+    reverseEndianness,
+    uint8ArrayToString,
+    stripHexPrefix,
+    reverseEndiannessHex,
+    parseRedeemRequestStatus,
+} from "../../../src/utils";
 import { H256Le } from "../../../src/interfaces/default";
+import {
+    InterbtcPrimitivesRedeemRedeemRequest,
+    InterbtcPrimitivesRedeemRedeemRequestStatus,
+} from "@polkadot/types/lookup";
 
 describe("Encoding", () => {
     let registry: TypeRegistry;
@@ -42,5 +53,119 @@ describe("Encoding", () => {
         const blockHashHexLE = "0x9067166e896765258f6636a082abad6953f17a0e8dc21fc4f85648ceeedbda69";
         const blockHashHexBE = "0x69dadbeece4856f8c41fc28d0e7af15369adab82a036668f256567896e166790";
         return assert.equal(reverseEndiannessHex(blockHashHexLE), stripHexPrefix(blockHashHexBE));
+    });
+
+    describe("parseRedeemRequestStatus", () => {
+        const buildMockStatus = (status: "Pending" | "Completed" | "Reimbursed" | "Retried") =>
+            <InterbtcPrimitivesRedeemRedeemRequestStatus>{
+                isPending: status === "Pending",
+                isCompleted: status === "Completed",
+                isReimbursed: status === "Reimbursed",
+                isRetried: status === "Retried",
+                type: status,
+            };
+
+        const buildMockRedeemRequest = (
+            status: InterbtcPrimitivesRedeemRedeemRequestStatus,
+            opentime?: number,
+            period?: number
+        ) => {
+            return <InterbtcPrimitivesRedeemRedeemRequest>{
+                period: registry.createType("u32", period),
+                opentime: registry.createType("u32", opentime),
+                status: status,
+            };
+        };
+
+        const assertEqualPretty = (expected: RedeemStatus, actual: RedeemStatus): void => {
+            assert.equal(actual, expected, `Expected '${RedeemStatus[expected]}' but was '${RedeemStatus[actual]}'`);
+        };
+
+        it("should correctly parse completed status", () => {
+            const mockRequest = buildMockRedeemRequest(buildMockStatus("Completed"));
+            const expectedStatus = RedeemStatus.Completed;
+
+            const actualStatus = parseRedeemRequestStatus(mockRequest, 42, 42);
+
+            assertEqualPretty(actualStatus, expectedStatus);
+        });
+
+        it("should correctly parse reimbursed status", () => {
+            const mockRequest = buildMockRedeemRequest(buildMockStatus("Reimbursed"));
+            const expectedStatus = RedeemStatus.Reimbursed;
+
+            const actualStatus = parseRedeemRequestStatus(mockRequest, 42, 42);
+
+            assertEqualPretty(actualStatus, expectedStatus);
+        });
+
+        it("should correctly parse retried status", () => {
+            const mockRequest = buildMockRedeemRequest(buildMockStatus("Retried"));
+            const expectedStatus = RedeemStatus.Retried;
+
+            const actualStatus = parseRedeemRequestStatus(mockRequest, 42, 42);
+
+            assertEqualPretty(actualStatus, expectedStatus);
+        });
+
+        describe("should correctly parse expired status", () => {
+            const currentBlock = 42;
+            const opentimeBlock = 1;
+            // pending status internally, but expect expired due to blocks elapsed
+            const mockInternalPendingStatus = buildMockStatus("Pending");
+            it("when global redeem period is larger than request period", () => {
+                const globalRedeemPeriod = 30;
+                const requestPeriod = 25;
+
+                const mockRequest = buildMockRedeemRequest(mockInternalPendingStatus, opentimeBlock, requestPeriod);
+                const expectedStatus = RedeemStatus.Expired;
+
+                const actualStatus = parseRedeemRequestStatus(mockRequest, globalRedeemPeriod, currentBlock);
+
+                assertEqualPretty(actualStatus, expectedStatus);
+            });
+
+            it("when global redeem period is smaller than request period", () => {
+                const globalRedeemPeriod = 25;
+                const requestPeriod = 30;
+
+                // pending status internally, but expect expired due to blocks elapsed
+                const mockRequest = buildMockRedeemRequest(mockInternalPendingStatus, opentimeBlock, requestPeriod);
+                const expectedStatus = RedeemStatus.Expired;
+
+                const actualStatus = parseRedeemRequestStatus(mockRequest, globalRedeemPeriod, currentBlock);
+
+                assertEqualPretty(actualStatus, expectedStatus);
+            });
+
+            it("when opentime + period is equal to current block", () => {
+                const globalRedeemPeriod = currentBlock - opentimeBlock;
+                // anything less than above
+                const requestPeriod = globalRedeemPeriod - 1;
+
+                // pending status internally, but expect expired due to blocks elapsed
+                const mockRequest = buildMockRedeemRequest(mockInternalPendingStatus, opentimeBlock, requestPeriod);
+                const expectedStatus = RedeemStatus.Expired;
+
+                const actualStatus = parseRedeemRequestStatus(mockRequest, globalRedeemPeriod, currentBlock);
+
+                assertEqualPretty(actualStatus, expectedStatus);
+            });
+        });
+
+        it("should correctly parse pending status", () => {
+            const currentBlock = 42;
+            const opentimeBlock = 1;
+            const globalRedeemPeriod = 50;
+            const requestPeriod = 25;
+
+            // pending status internally, and expect still pending due to blocks elapsed
+            const mockRequest = buildMockRedeemRequest(buildMockStatus("Pending"), opentimeBlock, requestPeriod);
+            const expectedStatus = RedeemStatus.PendingWithBtcTxNotFound;
+
+            const actualStatus = parseRedeemRequestStatus(mockRequest, globalRedeemPeriod, currentBlock);
+
+            assertEqualPretty(actualStatus, expectedStatus);
+        });
     });
 });
