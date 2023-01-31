04763740f79d44e6cde11020018a4e411366debd
Gregory Hill <gregorydhill@outlook.com>
2023-01-31 15:25:04 +0000

    chore: add liquidity to base and meta pools
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- scripts/init-testnet.ts
+++ scripts/init-testnet.ts
@@ -15,6 +15,11 @@ const readline = require("readline");
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 
+const treasuryAccount = Buffer.concat([
+    Buffer.from("modl"), // 4 bytes
+    Buffer.from("mod/trsy"), // 8 bytes
+], 32);
+
 const args = yargs(hideBin(process.argv))
     .option("parachain-endpoint", {
         description: "The wss url of the parachain",
@@ -41,7 +46,7 @@ function constructLendingSetup(api: ApiPromise) {
             {
                 Token: 'KBTC'
             },
-            { 
+            {
                 collateralFactor: 630000,
                 liquidationThreshold: 670000,
                 reserveFactor: 200000,
@@ -59,13 +64,13 @@ function constructLendingSetup(api: ApiPromise) {
                 state: "Pending",
                 supplyCap: "2000000000",
                 borrowCap: "2000000000",
-                lendTokenId: {LendToken: 1} 
+                lendTokenId: { LendToken: 1 }
             }
         ), api.tx.loans.addMarket(
             {
                 Token: 'KSM'
             },
-            { 
+            {
                 collateralFactor: 540000,
                 liquidationThreshold: 610000,
                 reserveFactor: 200000,
@@ -83,13 +88,13 @@ function constructLendingSetup(api: ApiPromise) {
                 state: "Pending",
                 supplyCap: "30000000000000000",
                 borrowCap: "30000000000000000",
-                lendTokenId: {LendToken: 2} 
+                lendTokenId: { LendToken: 2 }
             }
         ), api.tx.loans.addMarket(
             {
                 ForeignAsset: 1 // usdt
             },
-            { 
+            {
                 collateralFactor: 650000,
                 liquidationThreshold: 690000,
                 reserveFactor: 200000,
@@ -107,13 +112,13 @@ function constructLendingSetup(api: ApiPromise) {
                 state: "Pending",
                 supplyCap: "80000000000",
                 borrowCap: "80000000000",
-                lendTokenId: {LendToken: 3} 
+                lendTokenId: { LendToken: 3 }
             }
         ), api.tx.loans.addMarket(
             {
                 ForeignAsset: 2 // movr
             },
-            { 
+            {
                 collateralFactor: 470000,
                 liquidationThreshold: 560000,
                 reserveFactor: 200000,
@@ -131,73 +136,75 @@ function constructLendingSetup(api: ApiPromise) {
                 state: "Pending",
                 supplyCap: "20000000000000000000000",
                 borrowCap: "20000000000000000000000",
-                lendTokenId: {LendToken: 4} 
+                lendTokenId: { LendToken: 4 }
             }
         )
     ];
 
     const underlyingTokens = [
-        {Token: "KBTC"},
-        {Token: "KSM"},
-        {ForeignAsset: 1}, // usdt
-        {ForeignAsset: 2}, // movr
+        { Token: "KBTC" },
+        { Token: "KSM" },
+        { ForeignAsset: 1 }, // usdt
+        { ForeignAsset: 2 }, // movr
     ];
 
     let addRewards = [
         api.tx.utility.dispatchAs(
-            { system: { Signed:  "a3ckVDnZwjdBhkq1KJodZj3iCgYMseWfLA5fpqExuEpMy8Y5q" } }, // root
+            { system: { Signed: treasuryAccount } },
             api.tx.loans.addReward("100000000000000000000")
         )
     ];
-    let activateMarketWithRewards = underlyingTokens.map((token) => { return [
-        api.tx.loans.activateMarket(token),
-        api.tx.loans.updateMarketRewardSpeed(token, 10, 10),
-    ]}).reduce((x, y) => { return x.concat(y);});
+    let activateMarketWithRewards = underlyingTokens.map((token) => {
+        return [
+            api.tx.loans.activateMarket(token),
+            api.tx.loans.updateMarketRewardSpeed(token, 10, 10),
+        ]
+    }).reduce((x, y) => { return x.concat(y); });
 
     return addMarkets.concat(addRewards).concat(activateMarketWithRewards);
 }
 
 function constructFundingSetup(api: ApiPromise) {
     const tokens = [
-        {Token: "KSM"}, 
-        {Token: "KINT"},
-        {Token: "KBTC"}, // note: if the faucet gives this, it'll lead to unredeemable kbtc
+        { Token: "KSM" },
+        { Token: "KINT" },
+        { Token: "KBTC" }, // NOTE: this is unredeemable
         { ForeignAsset: 1 },
         { ForeignAsset: 2 },
         { ForeignAsset: 3 },
         { ForeignAsset: 4 },
-        { ForeignAsset: 5 } 
+        { ForeignAsset: 5 }
     ];
-    const faucetSetup = tokens.map((token) => {
-        return api.tx.tokens.setBalance(
-            "5DqzGaydetDXGya818gyuHA7GAjEWRsQN6UWNKpvfgq2KyM7",
-            token,
-            "1000000000000000000000000000", //1e24 planck
-            0
-        );
-    });
-    const calls = faucetSetup.concat([
-        api.tx.tokens.setBalance(
-            "a3cgeH7D28bBsHY4hGLzxkMFUcFQmjGgDa2kmxg3D9Z6AyhtL", // treasury
-            {Token: "KINT"},
-            "1000000000000000000000000000", //1e15 KINT
-            0
-        )
-    ]);
-    return calls;
+    return tokens.map((token) => {
+        return [
+            api.tx.tokens.setBalance(
+                // faucet account
+                "5DqzGaydetDXGya818gyuHA7GAjEWRsQN6UWNKpvfgq2KyM7",
+                token,
+                "1000000000000000000000000000", //1e24 planck
+                0
+            ),
+            api.tx.tokens.setBalance(
+                treasuryAccount,
+                token,
+                "1000000000000000000000000000", //1e15 KINT
+                0
+            )
+        ]
+    }).flat();
 }
 
 function constructVaultRegistrySetup(api: ApiPromise) {
     const currencyPair = {
-        collateral: {ForeignAsset: 3}, // lksm 
-        wrapped: {Token: "KBTC"}
+        collateral: { ForeignAsset: 3 }, // lksm 
+        wrapped: { Token: "KBTC" }
     };
     return [
         api.tx.vaultRegistry.setLiquidationCollateralThreshold(currencyPair, "1450000000000000000"),
-        api.tx.vaultRegistry.setPremiumRedeemThreshold(currencyPair, "1650000000000000000"), 
-        api.tx.vaultRegistry.setSecureCollateralThreshold(currencyPair, "1800000000000000000"), 
-        api.tx.vaultRegistry.setMinimumCollateral(currencyPair.collateral, "20000000000000"), 
-        api.tx.vaultRegistry.setSystemCollateralCeiling(currencyPair, "38000000000000000"), 
+        api.tx.vaultRegistry.setPremiumRedeemThreshold(currencyPair, "1650000000000000000"),
+        api.tx.vaultRegistry.setSecureCollateralThreshold(currencyPair, "1800000000000000000"),
+        api.tx.vaultRegistry.setMinimumCollateral(currencyPair.collateral, "20000000000000"),
+        api.tx.vaultRegistry.setSystemCollateralCeiling(currencyPair, "38000000000000000"),
     ];
 }
 
@@ -205,8 +212,11 @@ function constructAnnuitySetup(api: ApiPromise) {
     const blocksPerYears = 365 * 24 * 60 * 5; // 5 per minute
     const vaultAnnuity = [
         api.tx.tokens.setBalance(
-            "a3cgeH7D3w3wu37yHx4VZeae4EUqNTw5RobTp5KvcMsrPLWJg", // vaultAnnuity
-            {Token: "KINT"},
+            Buffer.concat([
+                Buffer.from("modl"), // 4 bytes
+                Buffer.from("vlt/annu"), // 8 bytes
+            ], 32),
+            { Token: "KINT" },
             new BN(102803978514).muln(blocksPerYears),
             0
         ),
@@ -214,8 +224,11 @@ function constructAnnuitySetup(api: ApiPromise) {
     ];
     const escrowAnnuity = [
         api.tx.tokens.setBalance(
-            "a3cgeH7CzXoGgXh453eaSJRCvbbBKZN4mejwUVkic8efQUi5R", // escrowAnnuity
-            {Token: "KINT"},
+            Buffer.concat([
+                Buffer.from("modl"), // 4 bytes
+                Buffer.from("esc/annu"), // 8 bytes
+            ], 32),
+            { Token: "KINT" },
             new BN(47564687975).muln(blocksPerYears),
             0
         ),
@@ -227,9 +240,9 @@ function constructAnnuitySetup(api: ApiPromise) {
 async function constructAmmSetup(api: ApiPromise) {
     // workaround for broken is_exists check - TODO: remove once fixed.
     // see https://github.com/interlay/interbtc/blob/1a1afa90228f37c9ade4acbda8275c2f5cfe85ce/parachain/runtime/testnet-kintsugi/src/zenlink.rs#L43
-    const isExistsWorkaround = [ 
-        {Token: "KBTC" }, // foreign assets also need issuance, but faucet already has those
-    ].map((token) => { 
+    const isExistsWorkaround = [
+        { Token: "KBTC" }, // foreign assets also need issuance, but faucet already has those
+    ].map((token) => {
         return api.tx.tokens.setBalance(
             "a3ckVDnZwjdBhkq1KJodZj3iCgYMseWfLA5fpqExuEpMy8Y5q", // root
             token,
@@ -237,7 +250,7 @@ async function constructAmmSetup(api: ApiPromise) {
             1
         )
     });
-    
+
     const pools = [
         [{ Token: "KBTC" }, { Token: "KSM" }, 45_000],
         [{ Token: "KBTC" }, { ForeignAsset: 1 }, 40_000], // usdt
@@ -245,19 +258,19 @@ async function constructAmmSetup(api: ApiPromise) {
         [{ Token: "KINT" }, { ForeignAsset: 2 }, 35_000], // movr
     ];
     const basicPoolSetup = pools.map(([token1, token2, reward]) => {
-            return [
-                api.tx.zenlinkProtocol.createPair(token1, token2),
-                api.tx.farming.updateRewardSchedule(
+        return [
+            api.tx.zenlinkProtocol.createPair(token1, token2),
+            api.tx.farming.updateRewardSchedule(
                 { LpToken: [token1, token2] },
                 { Token: "KINT" },
                 60 * 24 * 7 * 12, // three months, reward period is per minute
                 new BN(10).pow(new BN(12)).muln(reward as any),
             ),
-            ];
-    }).reduce((x, y) => { return x.concat(y);});
+        ];
+    }).reduce((x, y) => { return x.concat(y); });
 
-    const basePoolId = (await api.query.zenlinkStableAmm.nextPoolId() as any).toNumber(); // note: this is before the batch is executed
-
+    // note: this is before the batch is executed
+    const basePoolId = (await api.query.zenlinkStableAmm.nextPoolId() as any).toNumber();
     const basePoolSetup = [
         api.tx.zenlinkStableAmm.createBasePool(
             [
@@ -269,43 +282,67 @@ async function constructAmmSetup(api: ApiPromise) {
             200, // amplification coefficient
             100_000_000, // max fee 1%
             0, // no admin fee
-            Buffer.concat([
-                Buffer.from("modl"), // 4 bytes
-                Buffer.from("mod/trsy"), // 8 bytes
-            ], 32), // treasury
+            treasuryAccount,
             "LKSM+VKSM+SKSM" // currency symbol
-        ), api.tx.farming.updateRewardSchedule(
+        ),
+        api.tx.farming.updateRewardSchedule(
             { StableLpToken: basePoolId },
             { Token: "KINT" },
             60 * 24 * 7 * 12, // three months, reward period is per minute
             new BN(10).pow(new BN(12)).muln(15_000),
+        ),
+        api.tx.utility.dispatchAs(
+            { system: { Signed: treasuryAccount } },
+            api.tx.zenlinkStableAmm.addLiquidity(
+                basePoolId,
+                [
+                    100_000,
+                    100_000,
+                    100_000,
+                ],
+                0, // min mint amount
+                treasuryAccount, // recipient
+                new BN(4294967295), // deadline
+            ),
         )
     ];
 
+    const metaPoolId = basePoolId + 1;
     const metaPoolSetup = [
         api.tx.zenlinkStableAmm.createMetaPool(
             [
                 { Token: "KSM" },
                 { StableLpToken: basePoolId }, // LKSM+VKSM+SKSM
             ],
-            [12, 12], // decimals
+            [12, 18], // decimals
             200, // amplification coefficient
             100_000_000, // max fee 1%
             0, // no admin fee
-            Buffer.concat([
-                Buffer.from("modl"), // 4 bytes
-                Buffer.from("mod/trsy"), // 8 bytes
-            ], 32), // treasury
-            "(LKSM+VKSM+SKSM)+KSM" // currency symbol
-        ), api.tx.farming.updateRewardSchedule(
-            { StableLpToken: basePoolId + 1 },
+            treasuryAccount,
+            "KSM+(LKSM+VKSM+SKSM)" // currency symbol
+        ),
+        api.tx.farming.updateRewardSchedule(
+            { StableLpToken: metaPoolId },
             { Token: "KINT" },
             60 * 24 * 7 * 12, // three months, reward period is per minute
             new BN(10).pow(new BN(12)).muln(33_000),
+        ),
+        api.tx.utility.dispatchAs(
+            { system: { Signed: treasuryAccount } },
+            api.tx.zenlinkStableAmm.addLiquidity(
+                metaPoolId,
+                [
+                    100_000,
+                    100_000,
+                ],
+                0, // min mint amount
+                treasuryAccount, // recipient
+                new BN(4294967295), // deadline
+            ),
         )
     ];
 
-    return isExistsWorkaround.concat(basicPoolSetup).concat(basePoolSetup);//.concat(metaPoolSetup);
+    return isExistsWorkaround.concat(basicPoolSetup).concat(basePoolSetup).concat(metaPoolSetup);
 }
 
 function constructForeignAssetSetup(api: ApiPromise) {
@@ -314,7 +351,7 @@ function constructForeignAssetSetup(api: ApiPromise) {
             {
                 decimals: 6,
                 name: "Tether USD",
-                symbol: "USDT", 
+                symbol: "USDT",
                 existentialDeposit: 0,
                 location: null,
                 additional: { feePerSecond: 8153838, coingeckoId: "" }
@@ -324,7 +361,7 @@ function constructForeignAssetSetup(api: ApiPromise) {
             {
                 decimals: 18,
                 name: "Moonriver Token",
-                symbol: "MOVR", 
+                symbol: "MOVR",
                 existentialDeposit: 0,
                 location: null,
                 additional: { feePerSecond: 0, coingeckoId: "" }
@@ -334,7 +371,7 @@ function constructForeignAssetSetup(api: ApiPromise) {
             {
                 decimals: 12,
                 name: "Liquid KSM",
-                symbol: "LKSM", 
+                symbol: "LKSM",
                 existentialDeposit: 0,
                 location: {
                     V1: {
@@ -350,16 +387,15 @@ function constructForeignAssetSetup(api: ApiPromise) {
                             ]
                         }
                     }
-                    
                 },
                 additional: { feePerSecond: 233100000000, coingeckoId: "liquid-ksm" }
             },
-           3
+            3
         ), api.tx.assetRegistry.registerAsset(
             {
                 decimals: 12,
                 name: "Voucher KSM",
-                symbol: "VKSM", 
+                symbol: "VKSM",
                 existentialDeposit: 0,
                 location: {
                     V1: {
@@ -375,33 +411,32 @@ function constructForeignAssetSetup(api: ApiPromise) {
                             ]
                         }
                     }
-                    
                 },
                 additional: { feePerSecond: 233100000000, coingeckoId: "" }
             },
-           4
+            4
         ), api.tx.assetRegistry.registerAsset(
             {
                 decimals: 12,
                 name: "Staked KSM",
-                symbol: "SKSM", 
+                symbol: "SKSM",
                 existentialDeposit: 0,
                 location: null,
                 additional: { feePerSecond: 233100000000, coingeckoId: "" }
             },
-           5
+            5
         )
     ];
 }
 
 async function constructClientsInfoSetup(api: ApiPromise, baseUrl: String) {
     const checksumFile = await fetch(baseUrl + 'sha256sums.txt')
-    .then(res => {
-        if (res.status >= 400) {
-            throw new Error("Bad response from server");
-        }
-        return res.text();
-    });
+        .then(res => {
+            if (res.status >= 400) {
+                throw new Error("Bad response from server");
+            }
+            return res.text();
+        });
 
     const re = /([a-f0-9]+)\s*[.]\/(([a-z]+)-parachain-metadata-kintsugi-testnet)/g;
     let matches = []
@@ -433,13 +468,13 @@ async function setupParachain() {
 
     let calls = [
         await constructClientsInfoSetup(paraApi, args["clients-url"]),
-        constructFundingSetup(paraApi),
         constructForeignAssetSetup(paraApi),
+        constructFundingSetup(paraApi),
         constructLendingSetup(paraApi),
         constructVaultRegistrySetup(paraApi),
         constructAnnuitySetup(paraApi),
         await constructAmmSetup(paraApi),
-    ].reduce((x, y) => { return x.concat(y);});
+    ].reduce((x, y) => { return x.concat(y); });
 
     const batched = paraApi.tx.utility.batchAll(calls);
     const sudo = paraApi.tx.sudo.sudo(batched.method.toHex());
