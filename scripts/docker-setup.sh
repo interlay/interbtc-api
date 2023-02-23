196d092f772986c19fa1e8606d18ccfcd1f63c48
Brendon Votteler <bvotteler@users.noreply.github.com>
2023-02-23 10:22:27 +0100

    chore: bump clients to 1.19.7

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -23,7 +23,7 @@ if ! [ -d "local-setup" ]
 then
     mkdir local-setup
     git clone https://github.com/interlay/parachain-launch/
-    cd parachain-launch && git checkout 1.1.0-p1.21.10-c1.19.6 && yarn install
+    cd parachain-launch && git checkout 1.1.0-p1.21.10-c1.19.7 && yarn install
     yarn start generate --config=configs/kintsugi.yml --servicesPath=configs/kintsugi-services.yml --yes --output=local-setup-kint
     mv local-setup-kint ../local-setup/kint
     yarn start generate --config=configs/interlay.yml --servicesPath=configs/interlay-services.yml --yes --output=local-setup-intr
