0ebb62996644a9daa898a8943d0289846634cfb9
Brendon Votteler <bvotteler@users.noreply.github.com>
2023-08-22 13:32:39 +0800

    test: silence console.log in jest tests

--- test/utils/jestSetupFileAfterEnv.ts
+++ test/utils/jestSetupFileAfterEnv.ts
@@ -1,7 +1,14 @@
-import console from "console";
-
 // This file exists to override jest specific setup defaults
 
-// Replace jest's expanded logger the "stock" console to reduce verbosity of logging. 
-// Comment out this if you actually want expanded jest logging.
-global.console = console;
+// import console from "console";
+
+/**
+ * Replace jest's expanded logger the "stock" console to reduce verbosity of logging. 
+ * Needs `import console from "console"` to work.
+ */
+// global.console = console;
+
+/**
+ * Replace the logger with a silent spy, suppressing console.log outputs.
+ */
+jest.spyOn(console, "log").mockImplementation();
