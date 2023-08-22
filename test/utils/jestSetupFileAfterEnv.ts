784e3bd2f694a198907d6c93c30bcd71e757b6a8
Brendon Votteler <bvotteler@users.noreply.github.com>
2023-08-22 14:00:35 +0800

    test: enable logging again

--- test/utils/jestSetupFileAfterEnv.ts
+++ test/utils/jestSetupFileAfterEnv.ts
@@ -1,14 +1,14 @@
 // This file exists to override jest specific setup defaults
 
-// import console from "console";
+import console from "console";
 
 /**
  * Replace jest's expanded logger the "stock" console to reduce verbosity of logging. 
  * Needs `import console from "console"` to work.
  */
-// global.console = console;
+global.console = console;
 
 /**
  * Replace the logger with a silent spy, suppressing console.log outputs.
  */
-jest.spyOn(console, "log").mockImplementation();
+// jest.spyOn(console, "log").mockImplementation();
